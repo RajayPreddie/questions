c6eb5faa19ddc063176be27e547bed372598a971
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-06-13 03:15:05 -0400

    small changes

--- src/types/signinForm.ts
+++ src/types/signinForm.ts
@@ -1,9 +1,13 @@
-import { UseFormReturnType } from "@mantine/form";
-import { FormValues } from "./signin";
+import { type UseFormReturnType } from '@mantine/form';
+
+import { type FormValues } from './signin';
 
 export interface SigninFormProps {
   type: string;
   signInForm: UseFormReturnType<FormValues>;
-  onSubmit: (values: FormValues, event: React.FormEvent<HTMLFormElement> | undefined) => void;
+  onSubmit: (
+    values: FormValues,
+    event: React.FormEvent<HTMLFormElement> | undefined,
+  ) => void;
   toggle: () => void;
-  }
+}
